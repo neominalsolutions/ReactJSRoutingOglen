bb88c260d59e8696aa79aafd822c61a2fb00875f
mert.alptekin <mert.alptekin@neominal.com>
2022-02-23 18:56:12 +0300

    ReactReduxDevtools

--- src/store/mystore.js
+++ src/store/mystore.js
@@ -3,6 +3,7 @@ import thunk from 'redux-thunk';
 import { productReducer } from './reducers/product.reducer';
 import { cartReducer } from './reducers/cart.reducer';
 import { sideMenuReducer } from './reducers/side.menu.reducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 // asenkron bir yapıdan çekilen verinin araya girilip senkronlaştırılması için araya bir middleware ile gireririz. Burada bu işlemden sorumlu arkadaş thunk middleware
 // combineReducers ile sistemde ne kadar farklı state mekanizması takip edilecekse hepsini tanımlarız.
@@ -12,5 +13,7 @@ export const mystore = createStore(
 		cartState: cartReducer,
 		sideMenuState: sideMenuReducer,
 	}),
-	applyMiddleware(thunk)
+	composeWithDevTools(applyMiddleware(thunk))
 );
+
+// composeWithDevTools() ile redux devtools projeye dahil ettik.
